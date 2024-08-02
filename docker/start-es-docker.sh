e8c1e3b3ccbdf4d41594d62059e44c72207e8ece
HYS1753 <hys1753@naver.com>
2024-08-02 12:17:30 +0900

    feat: es test docker 환경 구성

---
+++ docker/start-es-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SOURCE_DIR="../build/distributions"
+CUSTOM_PLUGIN_DIR="./attachment"
+
+if [ -d "$CUSTOM_PLUGIN_DIR" ]; then
+  rm "$CUSTOM_PLUGIN_DIR"/*.zip
+  echo "Directory already exists. delete and remake directory: $CUSTOM_PLUGIN_DIR"
+else
+  echo "Make directory: $CUSTOM_PLUGIN_DIR"
+fi
+
+mkdir -p "$CUSTOM_PLUGIN_DIR"
+
+cp "$SOURCE_DIR"/* "$CUSTOM_PLUGIN_DIR"/
+
+if [ $? -eq 0 ]; then
+  echo "Custom plugin moved to docker volume successfully."
+  echo "docker run"
+  docker-compose -f ./docker-compse.yml up -d
+else
+  echo "Fail to move custom plugin, try gradle assemble to make custom plugin zip file"
+fi
+
